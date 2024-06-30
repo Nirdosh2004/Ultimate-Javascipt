ec06757e3c7cb6d7b05d06e2ea541a27e1a85f10
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-06-30 09:20:29 +0530

    what is up ?

---
+++ Logical_Operators.js
@@ -0,0 +1,20 @@
+const age = 10;
+const gender = 'female';
+
+if(age != 18 || gender == 'male'){    // '||' -> pipe operators bolte hai 
+          console.log(" You are Adult male");
+}
+else {
+          console.log("teenager");
+}
+
+
+// odd even problem
+
+const num = 57;
+if(num % 2 != 0){
+          console.log("Odd number");
+}
+else{
+          console.log("Even number");
+}
