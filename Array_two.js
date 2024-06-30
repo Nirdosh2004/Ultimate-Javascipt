287231e140bef3149cc7b8283a2565e1ee9b4613
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-06-30 17:29:18 +0530

    done

---
+++ Array_two.js
@@ -0,0 +1,28 @@
+// const students = ['nirdosh' , 'aishwary' , 'divyanshu'];
+
+
+// students.forEach((val) => {
+//           console.log(val + ' kush');
+// });
+
+
+// students.map((val) => {
+//           console.log(val);
+// })
+
+
+const num = [1,2,33,4,5];
+// let newArr = [];
+// num.map((num) => {
+//           newArr.push(num*2);
+// })
+
+// console.log(newArr);
+// console.log(num);
+
+// let ans = num.find(num => num === 33);
+// console.log(ans);
+
+
+let newar = num.filter(num => num %2 ==0)
+console.log(newar);
