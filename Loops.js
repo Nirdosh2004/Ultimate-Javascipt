5c0460cdea39274173970ec589d5d9047abe2c9d
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-06-30 16:14:43 +0530

    Done

---
+++ Loops.js
@@ -0,0 +1,18 @@
+for(let i=0;i<5;i++){
+          console.log("Nirdosh kushwaha");
+}
+
+
+let ii = 0;
+while(ii<=10){
+          console.log("Rishabh Kushwaha");
+          ii++;
+}
+
+
+
+let a = 0;
+do{
+          console.log("Hello");
+          a++;
+}while(a<=9)
